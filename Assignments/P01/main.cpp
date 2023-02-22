8906599b263f0faa9e2da8476565b3fcccc9bd3c
jstevens8185 <j.stevens8185@gmail.com>
2023-02-22 09:21:22 -0600

    Add Comments

--- Assignments/P01/main.cpp
+++ Assignments/P01/main.cpp
@@ -25,8 +25,25 @@
 
 /***
  * Gameplan:
- * Build employee array
-*/
+ * 
+ * Build an Employee. Employee will hold a value for each data point
+ * Build employee array and load all employees in from data file.
+ * Build binary tree node. Each node will hold an employee pointer for each data point
+ *  ie. a node will hold employee* ID, employee* first_name, ect.
+ * Build BST. Node 0 points to Employee array 0.
+ *  ie. Node0->ID = EmployeeArray[0], Node0->first_name = EmployeeArray[0]
+ *      Node1->ID = EmployeeArray[1], Node1->first_name = EmployeeArray[1], ect.
+ * To sort, compare Node0->ID to Node1->ID. Switch pointers if neccessary. 
+ *      Continue to sort all Node->ID until they are all in order.
+ * Next, compare Node[0]->first_name to Node[1]->first_name. Switch pointers if neccessary.
+ *      Sort all Node->first_name until they are all in order.
+ * Repeat with every data value.
+ * 
+ * After sorting is complete you should be left with a single binary search tree that has all
+ *      data values in a sorted order.
+ * Node[0]->ID may point to EmployeeArray[5] while Node[0]->first_name may point to EmployeeArray[526].
+ * 
+***/
 
 #include "json.hpp"
 #include <iostream>
