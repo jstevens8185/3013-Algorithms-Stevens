95c80e360ded6c80cf550f6b5f9f16f22add580e
Jered Stevens <j.stevens8185@gmail.com>
2023-02-22 08:37:38 -0600

    inti

---
+++ Assignments/P01/main.cpp
@@ -0,0 +1,40 @@
+/***
+ * build a data structure to sort and search employee files.
+ * The included data points are as follows:
+ * 
+ * int id
+ * string first_name
+ * string last_name
+ * string email
+ * string address
+ * string city
+ * string state
+ * double latitude
+ * double longitude
+ * string car
+ * string car_model
+ * string car_color
+ * string pet
+ * string job_title
+ * string phone_number
+ * vector <string> stocks
+ * 
+ * 
+***/
+
+
+/***
+ * Gameplan:
+ * Build employee array
+*/
+
+#include "json.hpp"
+#include <iostream>
+
+using json = nlohmann::json;
+
+int main()
+{
+    int i = 1;
+    std::cout << i;
+}
